51aaac2a4ff2ed81c107dc488ff3ac0cf7072278
opilane <opilane@HP-CZC6297B9B>
2022-09-02 11:34:45 +0300

    Add project files.

---
+++ Isik_inimene/Isik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isik_inimene
+{
+    abstract class Isik
+    {
+        protected string nimi;
+        protected int synd;
+        //protected char sugu;
+        protected enum Sugu { mees, naine};
+        protected Sugu sugu;
+
+        public Isik(/*char sugu = 'x'*/Sugu sugu, string nimi ="----", int synd = 0)
+        {
+            this.nimi = nimi;
+            this.synd = synd;
+            this.sugu = sugu;
+        }
+        public Isik(Isik ob)
+        {
+            this.nimi = ob.nimi;
+            this.synd = ob.synd;
+            this.sugu = ob.sugu;
+        }
+
+        public virtual void print()
+        {
+            Console.Write("{0,-10} {1} {2,4}", nimi, sugu, synd);
+        }
+        public int arvutaVanus()
+        {
+            int vanus = DateTime.Now.Year - synd;
+            return vanus;
+        }
+        public abstract double arvutaSissetulek(double maksuvaba, double tulumaks);
+    }
+}
